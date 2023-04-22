78fb27f2b85da18687a1ce68d9e0f56e5b3ef297
Panaite Cristian <panaite.cristian99@gmail.com>
2023-04-22 15:15:09 +0300

    landing page done

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,41 +1,58 @@
 import React from "react";
 
+const logo = require("./assets/logo.png");
+const desen = require("./assets/desen.png");
+
 function Home() {
   return (
     <div className="container">
-      <div className="navbar">
-        <div className="logo nav-item"></div>
-        <div className="nav-items">
-          <div className="nav-item black-bg">
-            <a href="#">Inchiriaza</a>
+      <div className="container1">
+        <div className="navbar">
+          <div className="logo nav-item">
+            <img src={logo} alt="logo" />
           </div>
-          <div className="nav-item">
-            <a href="#">Descopera</a>
-          </div>
-          <hr></hr>
-          <div className="nav-item">
-            <a href="#">Conectare</a>
-          </div>
-          <div className="nav-item black-bg">
-            <a href="#">Inscriere</a>
+          <span></span>
+          <span></span>
+          <span></span>
+
+          <div className="nav-items">
+            <div className="nav-item ">
+              <a href="/inchiriaza" className="black-bg">
+                Închiriază
+              </a>
+            </div>
+            <div className="nav-item">
+              <a href="/descopera">Descoperă</a>
+            </div>
+            <hr></hr>
+            <div className="nav-item">
+              <a href="/conectare">Conectare</a>
+            </div>
+            <div className="nav-item ">
+              <a href="/inscriere" className="black-bg">
+                Înscriere
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="heading">
-        <div className="left">
-          <div className="title">
-            <h1>BikeX</h1>
+        <div className="heading">
+          <div className="left">
+            <div className="title">
+              <h1>
+                Bike<span className="green">X</span>
+              </h1>
+            </div>
+            <div className="subtitle">
+              <h3>
+                Protejează mediul ! Închiriază o bicicletă sau trotinetă
+                electrică.
+              </h3>
+            </div>
           </div>
-          <div className="subtitle">
-            <h3>
-              Protejeaza mediul ! Inchiriaza o bicicleta sau trotineta
-              electrica.
-            </h3>
-          </div>
-        </div>
-        <div className="right">
-          <div className="desen">
-            <img src="" alt="desenu" />
+          <div className="right">
+            <div className="desen">
+              <img src={desen} alt="desenu" />
+            </div>
           </div>
         </div>
       </div>
@@ -44,7 +61,7 @@ function Home() {
         <div className="why_us">
           <div className="text_img">
             <div className="img">
-              <img src="" alt="image" />
+              <img src="" alt="image1" />
             </div>
             <div className="text">
               <h2>De ce noi ?</h2>
@@ -87,13 +104,16 @@ function Home() {
             </div>
           </div>
           <div className="how_img">
-            <img  src="" alt="img"/>
+            <img src="" alt="img" />
           </div>
         </div>
       </div>
 
       <div className="quote">
-        <h1>"Daca o cale e mai buna decat alta, atunci fii sigur ca e calea naturii." - Aristotel</h1>
+        <h1>
+          "Daca o cale e mai buna decat alta, atunci fii sigur ca e calea
+          naturii." - Aristotel
+        </h1>
       </div>
     </div>
   );
