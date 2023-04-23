487ff727ae1142c02b60628e787fc5e3b9034b1b
Panaite Cristian <panaite.cristian99@gmail.com>
2023-04-23 18:01:39 +0300

    login / register functionallity working

--- src/pages/Conectare.tsx
+++ src/pages/Conectare.tsx
@@ -1,20 +1,58 @@
 import React from "react";
 import "../styles/conectare.scss";
+import { useState } from "react";
+import axios from "axios";
 
 function Conectare() {
+  const [email, setEmail] = useState("");
+  const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const [pass, setPass] = useState("");
+
+  const handlePass = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPass(e.target.value);
+  };
+
+  const data = JSON.stringify({
+    email: email,
+    password: pass,
+  });
+
+  const Login = async () => {
+    // eslint-disable-next-line
+    const res = await axios
+      .post("http://localhost:4000/login", data, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      // .then((res) => {
+      //   t = res.data.token;
+      //   handleCookie();
+      //   if (cookies.token) {
+      //     navigateDashboard();
+      //   }
+      // });
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
+
   return (
     <div className="cm">
       <div className="form">
         <h1>Conectare</h1>
         <div className="input">
           <span className="eticheta">Email*</span>
-          <input type="email"></input>
+          <input type="email" required onChange={handleEmail}></input>
         </div>
         <div className="input">
           <span className="eticheta">Parolă*</span>
-          <input type="password"></input>
+          <input type="password" required onChange={handlePass}></input>
         </div>
-        <button>Conectare</button>
+        <button onClick={() => Login()}>Conectare</button>
         <h2>
           Nu ai deja cont ? Fă-ți unul <a href="/inscriere">aici.</a>
         </h2>
--- src/pages/Inscriere.tsx
+++ src/pages/Inscriere.tsx
@@ -1,20 +1,46 @@
 import React from "react";
 import "../styles/inscriere.scss";
+import { useState } from "react";
+import axios from "axios";
 
 function Inscriere() {
+  const [email, setEmail] = useState("");
+  const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const [pass, setPass] = useState("");
+
+  const handlePass = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPass(e.target.value);
+  };
+
+  const data = JSON.stringify({
+    email: email,
+    password: pass,
+  });
+
+  const Register = async () => {
+    // eslint-disable-next-line
+    const res = await axios.post("http://localhost:4000/register", data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
   return (
     <div className="cm">
       <div className="form">
         <h1>Înscriere</h1>
         <div className="input">
           <span className="eticheta">Email*</span>
-          <input type="email"></input>
+          <input type="email" required onChange={handleEmail}></input>
         </div>
         <div className="input">
           <span className="eticheta">Parolă*</span>
-          <input type="password"></input>
+          <input type="password" required onChange={handlePass}></input>
         </div>
-        <button>Înscriere</button>
+        <button onClick={() => Register()}>Înscriere</button>
         <h2>
           Ai deja cont ? Conectează-te <a href="/conectare">aici.</a>
         </h2>
